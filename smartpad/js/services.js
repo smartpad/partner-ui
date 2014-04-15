c76a604566d6bdf9d4d856ae5d46d0a5789e775c
vinhnguyenhoan <vinhnguyenhoan@gmail.com>
2014-04-15 09:01:00 +0700

    applying angularJS

--- smartpad/js/services.js
+++ smartpad/js/services.js
@@ -3,8 +3,36 @@
 /* Services */
 
 var userServices = angular.module('userServices', ['ngResource']);
-
+userServices.config(function($httpProvider) {
+	/*console.log("$httpProvider.defaults");
+	console.log($httpProvider.defaults);
+	console.log("$httpProvider.defaults.headers");
+	console.log($httpProvider.defaults.headers);
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];	
+	$httpProvider.defaults.useXDomain = true;
+	$httpProvider.defaults.headers.common = {};
+	$httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
+    $httpProvider.defaults.headers.post = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};*/
+});
+    
+/*userServices.factory('User', ['$resource',
+  function($resource){
+    return $resource('http://localhost:8090/acc', 
+	null, {send: {method:'POST', headers: {
+            'Content-Type': 'application/json'
+        }}}
+	)}]);*/
+  
 userServices.factory('User', ['$resource',
   function($resource){
-    return $resource('http://localhost:9090/userId=:userId&passwordId=:passwordId', {}, {});
-  }]);
+	//$resource.defaults.useXDomain = true;
+    return $resource('http://localhost:8090/acc', null,
+					{acc: {
+						method: 'POST',
+						headers: {
+						'Content-Type': 'application/json'
+					}}}
+	)
+}]);
