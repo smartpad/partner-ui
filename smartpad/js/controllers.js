8974b761c5d916a0a6a3c14110a6956021492788
vinhnguyenhoan <vinhnguyenhoan@gmail.com>
2014-04-28 01:08:07 +0700

    Login

--- smartpad/js/controllers.js
+++ smartpad/js/controllers.js
@@ -3,82 +3,41 @@
 /* Controllers */
 
 var smartpadControllers = angular.module('smartpadControllers', []);
-smartpadControllers.config(function($httpProvider) {
+/*smartpadControllers.config(function($httpProvider) {
 	console.log($httpProvider.defaults);
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];	
-	/*console.log("$httpProvider.defaults");
-	console.log($httpProvider.defaults);
-	console.log("$httpProvider.defaults.headers");
-	console.log($httpProvider.defaults.headers);
-	delete $httpProvider.defaults.headers.common['X-Requested-With'];	
-	$httpProvider.defaults.useXDomain = true;
-	$httpProvider.defaults.headers.common = {};
-	$httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-    $httpProvider.defaults.headers.post = {};
-    $httpProvider.defaults.headers.put = {};
-    $httpProvider.defaults.headers.patch = {};*/
-});
+});*/
 
-smartpadControllers.controller('LoginCtrl', ['$scope', '$routeParams', 'User', '$http',
-  function($scope, $routeParams, User, $http) {
+smartpadControllers.controller('LoginCtrl', ['$scope', '$routeParams', 'User', '$location',
+  function($scope, $routeParams, User, $location) {
     
     $scope.login = function() {
-	  /*$scope.user = User.get({userId: $scope.userNameText, passwordId: $scope.passwordText}, function(user) {
-		alert("user " + user);
-	  });*/
-	  //alert("user " + angular.toJson($scope.user));
-	  /*User.send({"data": angular.toJson($scope.user)}, {"userNameText": "lotte", "passwordText": "123abc"}, 
-				function(data,headers)
-                {
-                   alert('SAVED');
-                },
-                function(err,headers)
-                {
-					console.log(err)
-                    alert('FAILED ' + err + ' ' + headers);
-                });*/
-		
 		var user = new User();
 		user.userNameText = $scope.user.userNameText;
 		user.passwordText = $scope.user.passwordText;
-		//user.data = angular.toJson($scope.user);
 		user.data = $scope.user;
-		//console.log(user)
-		
-		delete $http.defaults.headers.common['X-Requested-With'];	
-		$http.post('http://localhost:8090/acc', $scope.user).error(function(data, status, headers, config) {
-			// called asynchronously if an error occurs
-			// or server returns response with an error status.
-			console.log(data)
-			console.log(status)
-			console.log(headers)
-			console.log(config)
-			//alert('FAILED ' + headers);
-		});
-		/*user.$acc({}, function(data, headers)
+
+		user.$acc({}, function(data, headers)
                 {
-				   console.log(data)
-                   alert('SAVED');
+					if (/*data.success*/ true) {
+						$location.path('/defaults')
+						return;
+					}
+                   alert('Account invalid: ' + data.data[0]);
                 },
                 function(err, headers)
                 {
-					console.log(err)
-                    alert('FAILED ' + err + ' ' + headers);
-                });*/
-		/*User.$acc({}, angular.toJson($scope.user), function(data, headers)
-                {
-				   console.log(data)
-                   alert('SAVED');
-                },
-                function(err, headers)
-                {
-					console.log(err)
-                    alert('FAILED ' + err + ' ' + headers);
-                });*/
+                    alert('Login failed: \'' + err + '\'!');
+                });
+
     }
   }]);
-
+smartpadControllers.controller('MainAppCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    // TODO
+	alert("accessed to main app");
+  }]);
 smartpadControllers.controller('RegistryCtrl', ['$scope', '$routeParams', 'User',
-  function($scope, $routeParams, Phone) {
+  function($scope, $routeParams, User) {
     // TODO
   }]);
--- smartpad/js/services.js
+++ smartpad/js/services.js
@@ -3,31 +3,9 @@
 /* Services */
 
 var userServices = angular.module('userServices', ['ngResource']);
-userServices.config(function($httpProvider) {
-	/*console.log("$httpProvider.defaults");
-	console.log($httpProvider.defaults);
-	console.log("$httpProvider.defaults.headers");
-	console.log($httpProvider.defaults.headers);
-	delete $httpProvider.defaults.headers.common['X-Requested-With'];	
-	$httpProvider.defaults.useXDomain = true;
-	$httpProvider.defaults.headers.common = {};
-	$httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-    $httpProvider.defaults.headers.post = {};
-    $httpProvider.defaults.headers.put = {};
-    $httpProvider.defaults.headers.patch = {};*/
-});
-    
-/*userServices.factory('User', ['$resource',
-  function($resource){
-    return $resource('http://localhost:8090/acc', 
-	null, {send: {method:'POST', headers: {
-            'Content-Type': 'application/json'
-        }}}
-	)}]);*/
-  
+ 
 userServices.factory('User', ['$resource',
   function($resource){
-	//$resource.defaults.useXDomain = true;
     return $resource('http://localhost:8090/acc', null,
 					{acc: {
 						method: 'POST',
